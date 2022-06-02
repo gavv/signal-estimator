2be1d68aec9e13b78ed619c9db3ecff5e5285b4a
Victor Gaydov <victor@enise.org>
2022-06-02 13:41:19 +0400

    Update readme_toc.sh

--- script/readme_toc.sh
+++ script/readme_toc.sh
@@ -1,4 +1,14 @@
 #! /bin/bash
 set -euo pipefail
-cd "$(dirname "$0")"
-markdown-toc -i ../README.md
+
+if ! which markdown-toc >/dev/null 2>&1; then
+    echo "error: markdown-toc not found in PATH" >&2
+    echo "you can install it using 'npm install --save markdown-toc'" >&2
+fi
+
+cd "$(dirname "$0")"/..
+
+cmd="markdown-toc -i README.md"
+echo "running $cmd"
+$cmd
+echo "done"
