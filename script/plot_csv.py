3f3cf5ed02c19fff5edc86703955b7d15bb6e1ed
Victor Gaydov <victor@enise.org>
2023-10-27 15:14:48 +0400

    Update plotting script

--- script/plot_csv.py
+++ script/plot_csv.py
@@ -1,29 +1,40 @@
 #!env python3
 import argparse
 import csv
+import numpy
 import os
 import pylab
 import sys
 
-def load(filename, device, stip_device, direction):
-    arr_x = []
-    arr_y = []
+def load(filename, direction, has_device, filter_device):
+    if filter_device:
+        print(f'Loading {direction} signal from {filename} for device {filter_device} ...',
+              file=sys.stderr)
+    else:
+        print(f'Loading {direction} signal from {filename} ...',
+              file=sys.stderr)
+    x_vals = []
+    y_vals = []
     with open(filename) as fp:
-        reader = csv.reader(fp, dialect='unix')
-        for row in reader:
-            if row[0] != direction:
+        for row in csv.reader(fp, dialect='unix'):
+            if len(row) < 2:
+                continue
+            if row[0] != direction[0]:
                 continue
             row = row[1:]
-            if device and row[0] != device:
-                continue
-            if device or stip_device:
+            if has_device:
+                if filter_device and row[0] != filter_device:
+                    continue
                 row = row[1:]
-            ts, amp = map(float, row[:2])
-            if ts == 0:
+            if len(row) < 2:
                 continue
-            arr_x.append(ts / 1000000)
-            arr_y.append(amp)
-    return arr_x, arr_y
+            ts = float(row[0])
+            amp = float(row[1])
+            if ts <= 0:
+                continue
+            x_vals.append(ts)
+            y_vals.append(amp)
+    return numpy.array([x_vals, y_vals]).T
 
 parser = argparse.ArgumentParser(description='plot dump(s) in csv format')
 
@@ -41,19 +52,32 @@ if len(args.dump) > 1:
 else:
     in_dump = out_dump
 if len(args.dump) > 2:
-    print('Too many arguments', file=sys.stderr)
-    exit(1)
+    parser.error('too many arguments, expected one or two dump files')
 
-stip_device = bool(args.device)
+has_device = bool(args.device)
 
-out_x, out_y = load(out_dump, None, stip_device, 'o')
-in_x, in_y = load(in_dump, args.device, stip_device, 'i')
+out_sig = load(out_dump, 'output', has_device, None)
+in_sig = load(in_dump, 'input', has_device, args.device)
+
+out_sig[:,0] /= 1000000
+in_sig[:,0] /= 1000000
+
+base = min(out_sig[0,0], in_sig[0,0])
+out_sig[:,0] -= base
+in_sig[:,0] -= base
+
+print("Plotting ...")
 
 fig = pylab.figure()
 fig.canvas.mpl_connect('close_event', lambda ev: os._exit(0))
 
-pylab.plot(out_x, out_y, '-o', label='output')
-pylab.plot(in_x, in_y, '-o', label='input')
+pylab.plot(out_sig[:,0], out_sig[:,1], '-o', label='output signal')
+pylab.plot(in_sig[:,0], in_sig[:,1], '-o',
+           label=(f'input signal ({args.device})' if has_device else 'input signal'))
+
+pylab.grid()
+pylab.xlabel("time, milliseconds")
+pylab.ylabel("amplitude")
 
 pylab.legend()
 pylab.show()
